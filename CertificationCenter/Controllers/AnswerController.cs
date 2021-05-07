c26e5413a48d57c3b682afd0f01012641670f0c6
LordAids <62069378+LordAids@users.noreply.github.com>
2021-05-08 00:08:40 +0300

    Добавил верстку Index
    На этой странице пользователь видит все свои пройденные тесты
    на данный момент это лишь заглушка

---
+++ CertificationCenter/Controllers/AnswerController.cs
@@ -0,0 +1,32 @@
+﻿using CertificationCenter.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CertificationCenter.Controllers
+{
+    public class AnswerController : Controller
+    {
+
+
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AnswerController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            //IEnumerable<>
+            return View();
+        }
+
+        
+    }
+}
