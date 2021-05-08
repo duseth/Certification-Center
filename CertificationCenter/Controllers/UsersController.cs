6d5372284b371ca7c4151a164a12bd5926ee1c77
duseth <ilyas.salimov.07@gmail.com>
2021-05-08 09:38:16 +0300

    docs(tutorial): добавить документирование для контроллер пользователей

--- CertificationCenter/Controllers/UsersController.cs
+++ CertificationCenter/Controllers/UsersController.cs
@@ -7,26 +7,44 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CertificationCenter.Controllers {
+    /// <summary>
+    /// Контроллер для управления пользователями.
+    /// </summary>
     [Authorize(Roles = "admin")]
     public class UsersController : Controller {
+        /// <summary>
+        /// Свойство предоставляющее постоянное хранилище объектам типа User и работу с ними.
+        /// </summary>
         private readonly UserManager<User> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UsersController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager) {
+        public UsersController(UserManager<User> userManager) {
             _userManager = userManager;
-            _roleManager = roleManager;
         }
 
+        /// <summary>
+        /// Обработчик GET-запроса при обращении на страницу пользователей.
+        /// </summary>
+        /// <returns>Страницу с пользователями.</returns>
         [HttpGet]
         public IActionResult Index() {
             return View(_userManager.Users.ToList());
         }
 
+        /// <summary>
+        /// Обработчик GET-запроса при обращении к странице с созданием пользователя.
+        /// </summary>
+        /// <returns>Страницу для создания пользователя.</returns>
         [HttpGet]
         public IActionResult Create() {
             return View();
         }
 
+        /// <summary>
+        /// Обработчик POST-запроса на создание пользователя.
+        /// </summary>
+        /// <param name="model">Модель представления для создания пользователя.</param>
+        /// <returns>Перенаправление на страницу пользователей при успешном добавлении,
+        /// иначе на страницу ошибки.</returns>
         [HttpPost]
         public async Task<IActionResult> Create(CreateUserViewModel model) {
             if (ModelState.IsValid) {
@@ -45,6 +63,11 @@ namespace CertificationCenter.Controllers {
             return View(model);
         }
 
+        /// <summary>
+        /// Обработчик GET-запроса при обращении на страницу изменения пользователя.
+        /// </summary>
+        /// <param name="id">Идентификатор пользователя.</param>
+        /// <returns>Страницу для изменения пользователя.</returns>
         [HttpGet]
         public async Task<IActionResult> Edit(string id) {
             User user = await _userManager.FindByIdAsync(id);
@@ -60,6 +83,12 @@ namespace CertificationCenter.Controllers {
             return View(model);
         }
 
+        /// <summary>
+        /// Обработчик POST-запроса на изменение пользователя.
+        /// </summary>
+        /// <param name="model">Модель представления для изменения пользователя.</param>
+        /// <returns>Перенаправление на страницу пользователей при успешном изменении,
+        /// иначе на страницу ошибки.</returns>
         [HttpPost]
         public async Task<IActionResult> Edit(EditUserViewModel model) {
             if (ModelState.IsValid) {
@@ -95,6 +124,11 @@ namespace CertificationCenter.Controllers {
             return View(model);
         }
 
+        /// <summary>
+        /// Обработчик POST-запроса на удаление пользователя.
+        /// </summary>
+        /// <param name="id">Идентификатор пользователя.</param>
+        /// <returns>Перенаправление на страницу пользователей.</returns>
         [HttpPost]
         public async Task<ActionResult> Delete(string id) {
             User user = await _userManager.FindByIdAsync(id);
