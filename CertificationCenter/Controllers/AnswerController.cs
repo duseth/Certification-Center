473904b86a714483dbd60bfa7ad6e507598023a1
LordAids <62069378+LordAids@users.noreply.github.com>
2021-05-10 11:22:34 +0300

    Добавил просмотр списка тестов для админа
    отредактировал верстку для пользователя

--- CertificationCenter/Controllers/AnswerController.cs
+++ CertificationCenter/Controllers/AnswerController.cs
@@ -1,6 +1,8 @@
 ﻿using CertificationCenter.Models;
+using CertificationCenter.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,22 +13,31 @@ namespace CertificationCenter.Controllers
     public class AnswerController : Controller
     {
 
-
+        private readonly ApplicationContext _db;
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AnswerController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        public AnswerController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, ApplicationContext db)
         {
             _userManager = userManager;
             _roleManager = roleManager;
+            _db = db;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index(EditUserViewModel model)
         {
-            //IEnumerable<>
-            return View();
+            User user = await _userManager.FindByIdAsync(model.Id);
+            IEnumerable<Answer> answers = _db.Answers.Where(a => a.UserId==user.Id);    //посмотреть, как вытаскивают юзер айди        
+            return View(answers);
         }
 
+        [HttpGet]
+        public IActionResult IndexAdm()
+        {
+            IEnumerable<Certification> certifications = _db.Certifications;
+            return View(certifications);
+        }
         
     }
 }
---
+++ CertificationCenter/ViewModels/AnswerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CertificationCenter.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CertificationCenter.ViewModels
+{
+    public class AnswerViewModel
+    {
+        public Answer answer { get; set; }
+        public IEnumerable<SelectListItem> AnswerList { get; set; }
+    }
+}
